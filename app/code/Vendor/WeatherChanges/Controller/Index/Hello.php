92237ae58617e299c46558cddb94fb8e9d31ca93
markuserkmann <markus.erkmann@voco.ee>
2025-05-20 17:14:08 +0300

    Created a way to read the API key without leaking it to Github

--- app/code/Vendor/WeatherChanges/Controller/Index/Hello.php
+++ app/code/Vendor/WeatherChanges/Controller/Index/Hello.php
@@ -4,13 +4,26 @@ namespace Vendor\WeatherChanges\Controller\Index;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\App\DeploymentConfig;
+
 
 class Hello extends Action
 {
+    protected $deploymentConfig;
+
+    public function __construct(
+        Context $context,
+        DeploymentConfig $deploymentConfig
+    ) {
+        parent::__construct($context);
+        $this->deploymentConfig = $deploymentConfig;
+    }
+
     public function execute()
     {
+        $cryptKey = $this->deploymentConfig->get('api/WeatherApiKey');
         $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
-        $result->setContents('Hello World');
+        $result->setContents('Hello World! The Weather API Key is: ' . $cryptKey);
         return $result;
     }
 }
