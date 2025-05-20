ad49df2f1c009b5c9b6b6f14ff294e034b91b724
markuserkmann <markus.erkmann@voco.ee>
2025-05-20 17:33:53 +0300

    Removed the API key, found a free api without a usage limit or an API key. The first one didn't work because it needed an hour or so for activating, second one used all of my tokens in one query.

--- app/code/Vendor/WeatherChanges/Controller/Index/Hello.php
+++ app/code/Vendor/WeatherChanges/Controller/Index/Hello.php
@@ -9,6 +9,7 @@ use Magento\Framework\App\DeploymentConfig;
 
 class Hello extends Action
 {
+    //Construct the class with the necessary dependencies
     protected $deploymentConfig;
 
     public function __construct(
@@ -19,11 +20,39 @@ class Hello extends Action
         $this->deploymentConfig = $deploymentConfig;
     }
 
-    public function execute()
-    {
-        $cryptKey = $this->deploymentConfig->get('api/WeatherApiKey');
-        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
-        $result->setContents('Hello World! The Weather API Key is: ' . $cryptKey);
+    function CallAPI($url) {
+        $curl = curl_init();
+
+        $url = sprintf("%s?%s", $url, http_build_query($data));
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
         return $result;
     }
+
+    public function execute()
+        {
+            $url = "https://api.open-meteo.com/v1/forecast?latitude=58.3806&longitude=26.7251&hourly=temperature_2m&forecast_days=1";
+            $response = $this->CallAPI('GET', $url);
+            $data = json_decode($response, true);
+
+            $currentHour = date('Y-m-d\TH:00', time());
+
+            $index = array_search($currentHour, $data['hourly']['time']);
+
+            if ($index !== false) {
+                $temp = $data['hourly']['temperature_2m'][$index];
+            } else {
+                $temp = 'N/A';
+            }
+
+            $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+            $result->setContents("Current temperature: {$temp}°C");
+            return $result;
+        }
 }
