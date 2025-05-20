08b0f6f20d6b9711770ffa44e5c64eacd09d1eff
markuserkmann <markus.erkmann@voco.ee>
2025-05-20 17:51:11 +0300

    Created a way to fetch all the current products and display them.

--- app/code/Vendor/WeatherChanges/Controller/Index/Hello.php
+++ app/code/Vendor/WeatherChanges/Controller/Index/Hello.php
@@ -5,25 +5,35 @@ use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\App\DeploymentConfig;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+
 
 
 class Hello extends Action
 {
     //Construct the class with the necessary dependencies
     protected $deploymentConfig;
+    protected $productCollectionFactory;
+
 
     public function __construct(
         Context $context,
-        DeploymentConfig $deploymentConfig
+        DeploymentConfig $deploymentConfig,
+        CollectionFactory $productCollectionFactory
     ) {
         parent::__construct($context);
         $this->deploymentConfig = $deploymentConfig;
+        $this->productCollectionFactory = $productCollectionFactory;
+
     }
 
-    function CallAPI($url) {
+    function CallAPI($url, $params = [])
+    {
         $curl = curl_init();
 
-        $url = sprintf("%s?%s", $url, http_build_query($data));
+        if (is_array($params) && !empty($params)) {
+            $url = sprintf("%s?%s", $url, http_build_query($params));
+        }
 
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -35,10 +45,32 @@ class Hello extends Action
         return $result;
     }
 
+    public function FetchAllTheProducts() 
+    {
+        $collection = $this->productCollectionFactory->create();
+        $collection->addAttributeToSelect('*');
+
+        $products = [];
+        foreach ($collection as $product) {
+            $products[] = [
+                'id' => $product->getId(),
+                'name' => $product->getName(),
+                'price' => $product->getPrice()
+            ];
+        }
+        return $products;
+    }
+    
+    // Retrieves all the products, generates a random float between 0.5 and 1.5, and multiplies the product price by the current temperature. Chooses randomly if it increases or decreases the product price. 
+    public function ChangePricesBasedOnTemperature () 
+    {
+
+    }
+
     public function execute()
         {
             $url = "https://api.open-meteo.com/v1/forecast?latitude=58.3806&longitude=26.7251&hourly=temperature_2m&forecast_days=1";
-            $response = $this->CallAPI('GET', $url);
+            $response = $this->CallAPI($url);
             $data = json_decode($response, true);
 
             $currentHour = date('Y-m-d\TH:00', time());
@@ -51,8 +83,17 @@ class Hello extends Action
                 $temp = 'N/A';
             }
 
+            $products = $this->FetchAllTheProducts();
+
+            $output = "Current temperature: {$temp}°C<br><br>";
+            $output .= "<strong>Products:</strong><br>";
+            foreach ($products as $product) {
+                $output .= "ID: {$product['id']},  Name: {$product['name']}, Price: {$product['price']}<br>";
+            }
+
+
             $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
-            $result->setContents("Current temperature: {$temp}°C");
+            $result->setContents($output);
             return $result;
         }
 }
